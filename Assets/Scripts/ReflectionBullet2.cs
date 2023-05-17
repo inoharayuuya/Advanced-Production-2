99463a273879a6b4327a878227e4c19c2795e086
Yuuya22 <yuuya.x151219xx@gmail.com>
2023-05-17 15:53:15 +0900

    弾があった時のHPの減少処理の追加、 プレイヤーのクラスを追加。

--- Assets/Scripts/ReflectionBullet2.cs
+++ Assets/Scripts/ReflectionBullet2.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class ReflectionBullet2 : MonoBehaviour
 {
+    GameObject playerClass;
+    PlayerClass player;
     [SerializeField]
     GameObject bullet;
     public float cnt = 0;
@@ -12,6 +14,8 @@ public class ReflectionBullet2 : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         bulletTransform = transform;
+        playerClass = GameObject.Find("PlayerClass");
+        player = playerClass.GetComponent<PlayerClass>();
     }
     private void Update()
     {
@@ -52,6 +56,8 @@ public class ReflectionBullet2 : MonoBehaviour
         }
         if (collision.gameObject.CompareTag("Player"))
         {
+            player.g_p1_hp -= player.p1_attack;
+            Debug.Log(player.g_p1_hp);
             Destroy(bullet);
         }
     }
