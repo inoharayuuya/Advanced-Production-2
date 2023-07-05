7a2ac0ed077ec7a4bd843ffd5de4c7526123dac3
Yuuyainohara <yuuya.ino.1219@gmail.com>
2023-07-05 12:39:37 +0900

    [StrixSyncField]の追加

--- Assets/Scripts/PlayerClass.cs
+++ Assets/Scripts/PlayerClass.cs
@@ -1,3 +1,4 @@
+using SoftGear.Strix.Unity.Runtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,14 +11,14 @@ public class PlayerClass : MonoBehaviour
 
     #region  パブリック
     // プレイヤー1
-    [SerializeField] public int p1_attack = 10;  // プレイヤー1の攻撃力
-    [SerializeField] public int p1_hp = 100;     // プレイヤー1のHP
-    public int g_p1_hp;
+    [StrixSyncField] public int p1_attack = 10;  // プレイヤー1の攻撃力
+    [StrixSyncField] public int p1_hp = 100;     // プレイヤー1のHP
+    [StrixSyncField] public int g_p1_hp;
 
     // プレイヤー2
-    [SerializeField] public int p2_attack = 10;  // プレイヤー2の攻撃力
-    [SerializeField] public int p2_hp = 100;     // プレイヤー2のHP
-    public int g_p2_hp;
+    [StrixSyncField] public int p2_attack = 10;  // プレイヤー2の攻撃力
+    [StrixSyncField] public int p2_hp = 100;     // プレイヤー2のHP
+    [StrixSyncField] public int g_p2_hp;
     #endregion
 
     private void Start()
