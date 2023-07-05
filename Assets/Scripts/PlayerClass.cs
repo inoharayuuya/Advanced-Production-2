3500db7dcc876485a8840f4859e6de882c3fd615
Yuuyainohara <yuuya.ino.1219@gmail.com>
2023-07-05 13:07:51 +0900

    Strixのクラスを複製処理

--- Assets/Scripts/PlayerClass.cs
+++ Assets/Scripts/PlayerClass.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerClass : MonoBehaviour
+public class PlayerClass : StrixBehaviour
 {
     #region  プライベート
 
